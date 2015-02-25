3ddb522f53e326049af819fe2d2017f87a8d07fa
Adriean Khisbe <adriean.khisbe@live.fr>
2015-02-25 17:16:14 +0100

    web: set up heroku

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Enable to store link, to which you can attach tag so that you miss any link.
 <!-- Yes I know i pocket like light :p -->
 
 See Raml api specification [here :) ](api-raml.yaml)
+
+
+You can consult the api on [Heroku](https://pending-link.herokuapp.com/)
+
+
+### Client Generation
+
+Raml tool example:
+npm install raml-client-generator -g
+raml-to-client api.raml -o api-client -l javascript
--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // Pending Link
+/* Premier Serveur */
 
-console.warn('Pending Link in building, come back later !');
+var http = require('http');
+var MESSAGE = 'Pending Link in building, come back later !';
+
+var server = http.createServer(function(req,rep) {
+    rep.writeHead(200);
+    rep.end(MESSAGE);
+});
+
+console.warn(MESSAGE);
+var port = process.env.PORT || 12121;
+server.listen(port);
+console.log("Listening on port "+port);
