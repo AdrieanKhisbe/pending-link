aa98a77a14b32a5c13435197c5053cfbbb364c1d
Adriean Khisbe <adriean.khisbe@live.fr>
2015-02-26 19:44:50 +0100

    dev: first api draft

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,9 @@
  *
  * Created by abecchis on 26/02/15.
  */
+var LinkController = require('../controller/linkController')
+
+
 module.exports = [
     {
         method: 'GET',
@@ -11,6 +14,16 @@ module.exports = [
             res("Hello Links!")
         }
 
+    },
+    {
+        method: 'GET',
+        path: '/api/links',
+        handler:LinkController.get
+    },
+    {
+        method: 'POST',
+        path: '/api/links',
+        handler:LinkController.create
     }
 
     ]
---
+++ src/controller/linkController.js
@@ -0,0 +1,28 @@
+/** Link Controllers
+ * Created by abecchis on 26/02/15.
+ */
+"use strict";
+
+var Hapi = require('hapi');
+var LinkDAO = require('../model/linkDao');
+var Link = require('../model/link');
+
+function LinkController(){};
+LinkController.prototype = (function(){
+
+    //TODO: see content type
+    return {
+        "get": function (request, reply) {
+            reply(LinkDAO.all());
+        },
+        create: function (request, reply) {
+            // TODO: stub
+            var l = Link.create("TMP")
+            //TODO Log
+            console.log(l);
+            LinkDAO.save(l)
+            reply().code(200);
+        }
+    }})();
+
+module.exports = new LinkController();
