2441cf26d86a668f3e24fdd9b60623acfb131731
Adriean Khisbe <adriean.khisbe@live.fr>
2015-02-26 18:48:22 +0100

    dev: add hapi server

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 // Pending Link
-/* Premier Serveur */
-
-var http = require('http');
 
 var log = require('bunyan').createLogger({name: 'pending-link'});
+
+var Hapi = require('hapi');
+var port = ~~process.env.PORT || 12121;
+
+var server = new Hapi.Server();
+server.connection({ port: port });
+
 var MESSAGE = 'Pending Link in building, come back later !';
-
-var server = http.createServer(function(req,rep) {
-    rep.writeHead(200);
-    rep.end(MESSAGE);
-});
-
 log.warn(MESSAGE);
-var port = process.env.PORT || 12121;
-server.listen(port);
-log.info("Listening on port "+port);
+server.start(function() {
+    log.info("Listening on port " + port);
+});
