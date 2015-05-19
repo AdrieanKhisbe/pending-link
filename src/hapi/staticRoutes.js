77838544c99c4795a87dc338ec2c57e7031a4c2f
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-19 10:12:18 +0200

    dev: add hapi route to serve the spec directory

--- src/hapi/staticRoutes.js
+++ src/hapi/staticRoutes.js
@@ -8,5 +8,6 @@
 module.exports =  [
     // index and doc
     {method: 'GET', path: '/', handler: {file: "public/index.html"}},
-    {method: 'GET', path: '/doc', handler: {file: "public/api.html"}}
+    {method: 'GET', path: '/doc', handler: {file: "public/api.html"}},
+    {method: 'GET', path: '/doc/{param*}', handler: { directory: {path: 'spec', listing: true}}}
   ];
