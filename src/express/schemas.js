ded731d470bbf3763f8dcf6f6865b19d175e505b
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-17 14:04:02 +0200

    refactor: add paperwork schema

---
+++ src/express/schemas.js
@@ -0,0 +1,35 @@
+/**
+ * Links Schemas
+ * Created by abecchis on 28/04/15.
+ */
+'use strict';
+
+var paperwork = require('paperwork');
+var schemas = {
+
+  linkCreate: {
+    url: String,
+    comment: paperwork.optional(String),
+    tags: paperwork.optional([String])
+  },
+
+  linkUpdate: {
+    url: String,
+    timestamp: Date,
+    archive: Boolean,
+    comment: String,
+    tags: [String],
+    type: /link/
+  },
+
+  linkPatch: {
+    url: paperwork.optional(String),
+    timestamp: paperwork.optional(Date),
+    archive: paperwork.optional(Boolean),
+    comment: paperwork.optional(String),
+    tags: paperwork.optional([String]),
+    type: paperwork.optional(/type/)
+  }
+};
+
+module.exports = schemas;
